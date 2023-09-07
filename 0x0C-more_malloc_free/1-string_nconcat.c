e3f8926bb9c978c842d29c1d500eb6f2975691a9
ubonisrael <akpanudohubonisrael@gmail.com>
2023-09-07 12:08:48 -0700

    0x0C correction2 task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (str == NULL)
 		return (NULL);
