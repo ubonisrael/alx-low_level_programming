cbccb3c6db0fbac3436fabbf9abfed9cc7b76508
ubonisrael <akpanudohubonisrael@gmail.com>
2023-09-07 15:24:43 -0700

    0x0C corrections task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _memset - sets block of memory to a specific value
+ * @ptr: pointer to memory block
+ * @b: value
+ * @size: size of memory block
+ * Return: void
+ */
+
+void _memset(char *ptr, int b, unsigned int size)
+{
+	unsigned int x;
+
+	for(x = 0; x < size; x++)
+		ptr[x] = b;
+}
+
+/**
  * _calloc - allocates memory for an array
  * @nmemb: element in array
  * @size: size of array
@@ -9,12 +25,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
+	_memset(ptr, 0, size * nmemb);
 	return (ptr);
 }
