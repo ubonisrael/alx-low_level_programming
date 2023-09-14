1ad716f7b248f8b7d44bb148a5a3b2b6709901eb
ubonisrael <akpanudohubonisrael@gmail.com>
2023-09-14 03:25:26 -0700

    0x10 initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all of its parameters
+ * @n: number of parameters
+ * Return: the sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int x;
+	int sum = 0;
+	va_list args;
+	va_start(args, n);
+
+	if (n == 0)
+		return (sum);
+	for (x = 0; x < n; x++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by new line
+ * @separator: string used to separate the integers
+ * @n: number of numbers
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list args;
+	va_start(args, n);
+
+	if (separator != NULL && n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			printf("%d", va_arg(args, int));
+			if (x < n - 1)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
