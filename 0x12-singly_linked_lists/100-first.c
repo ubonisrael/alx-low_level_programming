6b91c7874f6ed3419198222782c9428a511a0de7
ubonisrael <akpanudohubonisrael@gmail.com>
2023-09-21 11:36:31 -0700

    0x12 adv task 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * premain - prints something befre main is executed
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
