d0e99d783b2d9487075968948ddf1771710496e7
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-12 03:05:34 -0800

    0x17 task 7 corrections

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	if (*h == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	prev_node = (*h)->prev;
 	while (curr != NULL)
 	{
