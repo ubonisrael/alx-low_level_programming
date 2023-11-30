2b3386cb584c20ce31f25204e2e82480bf9a4905
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-30 09:32:58 -0800

    0x1A task 0 correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int x = 0;
 	hash_table_t *table;
 
-	if (size == 0)
-		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
