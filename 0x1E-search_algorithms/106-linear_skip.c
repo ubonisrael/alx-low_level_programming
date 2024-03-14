84f7443af5fdb5a28b75039022802d80312fd246
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-14 04:45:53 +0100

    0x1E task 13 initial attempt

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+  * linear_skip - searches for a value in a sorted skiplinked list
+  * @list: pointer to head of a list
+  * @value: target value
+  * Return: pointer to first node that holds value else NULL
+  */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current, *i, *j;
+
+	if (list == NULL)
+		return (NULL);
+	current = i = j = list;
+	current = current->express;
+	if (current)
+		j = current;
+	while (current && current->n < value)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
+		current = current->express;
+		if (current)
+		{
+			i = j;
+			j = current;
+		}
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i->index, j->index);
+	while (i && i->index <= j->index)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", i->index, i->n);
+		if (i->n == value)
+			return (i);
+		i = i->next;
+	}
+	return (NULL);
+}
