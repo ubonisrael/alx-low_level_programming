3548670c4314ec14313fdd12426144021b46c6dd
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-19 14:20:42 +0100

    0x1E corrected task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,5 +1,7 @@
 #include "search_algos.h"
 
+listint_t *jump_util(listint_t *list, int jump_factor, listint_t **j);
+
 /**
   * jump_list - searches for a value in a linked list using
   * Jump search Algorithm
@@ -11,35 +13,22 @@
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	int jump_value, tmp;
+	int jump_value;
 	listint_t *current, *i, *j;
 
 	if (list == NULL)
 		return (NULL);
-	tmp = jump_value = (int)sqrt(size);
+	jump_value = (int)sqrt(size);
 	current = i = j = list;
-	while (tmp > 0 && current != NULL)
-	{
-		current = current->next;
-		if (current)
-			j = current;
-		tmp--;
-	}
-	tmp = jump_value;
+	current = jump_util(list, jump_value, &j);
+	printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
 	while (current != NULL)
 	{
 		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
 		if (current->n >= value || current->index >= size)
 			break;
 		i = j;
-		while (tmp > 0 && current != NULL)
-		{
-			current = current->next;
-			if (current)
-				j = current;
-			tmp--;
-		}
-		tmp = jump_value;
+		current = jump_util(current, jump_value, &j);
 	}
 	printf("Value found between indexes [%ld] and [%ld]\n", i->index, j->index);
 	while (i && i->index <= j->index && i->index < size)
@@ -50,4 +39,30 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		i = i->next;
 	}
 	return (NULL);
+}
+
+/**
+  * jump_util - jumps "n" number of nodes
+  * @list: pointer to linked list
+  * @jump_factor: number of nodes to jump
+  * @j: pointer to node at which jump process currently on
+  * Return: pointer to node jumped to or NULL if it doesn't exist
+  */
+
+listint_t *jump_util(listint_t *list, int jump_factor, listint_t **j)
+{
+	listint_t *tmp = list;
+
+	while (jump_factor > 0 && tmp != NULL)
+	{
+		if (tmp->next == NULL)
+			printf("Value checked at index [%ld] = [%d]\n",
+					tmp->index, tmp->n);
+		tmp = tmp->next;
+		if (tmp)
+			*j = tmp;
+		*j = *j;
+		jump_factor--;
+	}
+	return (tmp);
 }
